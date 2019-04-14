3963ca3167fc4bd70aefe9896acf2470d702b957
Eddie Liu <liux1704@umn.edu>
2019-04-14 16:38:05 -0500

    Reformatted, removed measure line code and added structure for notes objects

--- clef.py
+++ clef.py
@@ -1,5 +1,6 @@
 import turtle
 import math
+import re
 
 
 def drawline(t, length):
@@ -120,10 +121,48 @@ def flatoval(turtle, r):  # Horizontal Oval
         draw_partof_circle(turtle, r / 2, 0.25, True, 4, 1, 25)
 
 
+def read_notes(notestring, x_start=70, x_spacing=30, y_spacing=10):
+    """ Reads in a string from ABC-notation file and creates note objects.
+    Does not handle headers at the moment.
+    """
+    header = re.match(r'.:', notestring)
+    if header:  # Headers are currently not supported
+        return None
+    else:
+        measure_list = notestring.split('|')
+        pattern = re.compile(r'''
+        
+        (_?[_=^]?\^?                 # accidentals (optional)
+        [a-g]                        # each note has a letter
+        [,']?)                       # followed by punctuation for low or high octave (optional)
+        (/?\d*)                      # followed by length modifier (optional, also extract length for spacing)
+        
+        ''', flags=re.IGNORECASE | re.VERBOSE)
+        note_names = ('C,', 'D,', 'E,', 'F,', 'G,', 'A,', 'B,',
+                      'C', 'D', 'E', 'F', 'G', 'A', 'B',
+                      'c', 'd', 'e', 'f', 'g', 'a', 'b',
+                      "c'", "d'", "e'", "f'", "g'", "a'", "b'")
+        y_values = range(-9 * y_spacing, 19 * y_spacing, y_spacing)
+        treble_dict = dict(zip(note_names, y_values))
+        # print(treble_dict)
 
-def read_notes(notestring):
-    pass
-# for char in notestring:
+        for measure in measure_list:
+            note_charlist = list(filter(None, re.findall(pattern, measure)))
+            note_charlist.append('|')
+            print(note_charlist)
+            for note in note_charlist:
+                if note == '|':
+                    # new_measure(x_start)
+                    pass
+                else:
+                    print('{}-->{}'.format(note, treble_dict.get(note[0], 0)))
+                    # new_note(x_start, treble_dict.get(note[0], 0), 'quarter')
+                    pass
+
+                x_start += x_spacing
+
+    return
+
 
 # return
 def main():
@@ -139,8 +178,8 @@ def main():
     draw_5_line(liner, x_max - border, 20)
 
     # draw the clef
-    arcy = turtle.Turtle()
-    draw_clef(arcy, 10, 25, -20)
+    # arcy = turtle.Turtle()
+    # draw_clef(arcy, 10, 25, -20)
 
     # read in a notestring and create note objects
     with open('test_notes.txt', 'r') as myfile:
@@ -148,15 +187,15 @@ def main():
             list_of_notes = read_notes(line)
 
     # draw measurelines and notes
-    # notes('')
     player = turtle.Turtle()
     player.ht()
     player.speed(0)
     player.width(2)
     # for notes in list_of_notes:
-        # notes.draw()
+    # notes.draw()
 
     s.exitonclick()
+
 
 if __name__ == '__main__':
     main()
